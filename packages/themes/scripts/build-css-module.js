00e5d8237b6e6b93c679a8b9993ddc4cf0fd2101
BORA <a52447879@gmail.com>
2024-05-26 22:02:12 +0900

    feat: theme

--- packages/themes/scripts/build-css-module.js
+++ packages/themes/scripts/build-css-module.js
@@ -1,7 +1,96 @@
 import * as theme from "../dist/index.js";
+import fs from "fs";
 
 /** CSS 파일 만들기: Object CSS 제네레이트 */
 
-Object.entries(theme.vars).forEach(([key, value]) => {
-  console.log(key, value); // colors { '$scale': [Getter], '$static': [Getter] }
-});
+// theme.css
+// :root {
+//   --gray-900: #171923
+// }
+
+const toCssCasting = (str) => {
+  return str
+    .replace(/([a-z])(\d)/, "$1-$2")
+    .replace(/([A-Z])/g, "-$1")
+    .toLowerCase();
+};
+
+const generateThemeCssVariables = () => {
+  const cssString = [];
+
+  Object.entries(theme.vars).forEach(([key, value]) => {
+    if (key === "colors") {
+      Object.entries(value.$static).forEach(([colorKey, colorValue]) => {
+        if (colorKey === "light") {
+          const selector = ":root";
+
+          const cssVariables = Object.entries(colorValue)
+            .map(([mainKey, mainValue]) =>
+              Object.entries(mainValue)
+                .map(
+                  ([subKey, subValue]) =>
+                    `--${toCssCasting(mainKey)}-${toCssCasting(
+                      subKey
+                    )}: ${subValue};`
+                )
+                .join("\n")
+            )
+            .join("\n");
+
+          return cssString.push(`${selector} {\n${cssVariables}\n}`);
+        }
+
+        if (colorKey === "dark") {
+          const selector = ":root .theme-dark";
+
+          const cssVariables = Object.entries(colorValue)
+            .map(([mainKey, mainValue]) =>
+              Object.entries(mainValue)
+                .map(
+                  ([subKey, subValue]) =>
+                    `--${toCssCasting(mainKey)}-${toCssCasting(
+                      subKey
+                    )}: ${subValue};`
+                )
+                .join("\n")
+            )
+            .join("\n");
+
+          return cssString.push(`${selector} {\n${cssVariables}\n}`);
+        }
+      });
+
+      return;
+    }
+
+    const selector = ":root";
+
+    const cssVariables = Object.entries(value)
+      .map(([mainKey, mainValue]) =>
+        Object.entries(mainValue)
+          .map(
+            ([subKey, subValue]) =>
+              `--${toCssCasting(mainKey)}-${toCssCasting(subKey)}: ${subValue};`
+          )
+          .join("\n")
+      )
+      .join("\n");
+
+    return cssString.push(`${selector} {\n${cssVariables}\n}`);
+  });
+  return cssString;
+};
+
+// .headingxl {
+//   font-size: 3rem;
+//   font-weight: 700;
+//   line-height: 100%;
+// }
+
+const generateThemeCss = () => {
+  const variables = generateThemeCssVariables();
+
+  fs.writeFileSync("dist/themes.css", [...variables].join("\n"));
+};
+
+generateThemeCss();
