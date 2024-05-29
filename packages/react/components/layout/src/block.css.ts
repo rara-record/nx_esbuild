6bd2ab1bed100ed40c190679a210e52b8196d73c
BORA <a52447879@gmail.com>
2024-05-29 19:54:29 +0900

    feat: setting layout packages

---
+++ packages/react/components/layout/src/block.css.ts
@@ -0,0 +1,9 @@
+import { style } from "@vanilla-extract/css";
+import { vars } from '@repo/themes'
+
+
+export const blockStyle = style({
+  width: '100px',
+  height: '100px',
+  color: `${vars.colors.$scale.gray[100]}`
+})
